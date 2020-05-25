d738b69b49c1d1c60c4b76e896e44a748f29798a
Alex Garrity <garrity2799@gmail.com>
2020-05-26 00:54:45 +0100

    Removed system call from build script

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from sys import argv
-from os import system
+from argparse import ArgumentParser
+from importlib import util
 
 
-def check_project_structure(src_directory : Path, gfx_directory : Path, lang_directory : Path):
+def check_project_structure(src_directory: Path, gfx_directory: Path,
+                            lang_directory: Path):
     # Check that the project is properly structured
     if not src_directory.exists():
         print("\"src\" directory not found.  Aborting")
@@ -12,24 +14,31 @@ def check_project_structure(src_directory : Path, gfx_directory : Path, lang_dir
         print("\"gfx\" directory not found.  Aborting")
         return -1
     if not lang_directory.exists():
-        print("\"lang\" directory not found.  Assuming hard-coded strings (this is not best practice)")
-    
+        print(
+            "\"lang\" directory not found.  Assuming hard-coded strings (this is not best practice)"
+        )
 
     # Find the grf, railtypes, and templates files
     if not src_directory.joinpath("grf.pnml").exists():
-        print("\"grf.pnml\" not found.  It should be in \"src\" and contain the grf block")
+        print(
+            "\"grf.pnml\" not found.  It should be in \"src\" and contain the grf block"
+        )
         return -1
     if not src_directory.joinpath("railtypes.pnml").exists():
-        print("\"railtypes.pnml\" not found.  It should be in \"src\" and contain the railtypetable block")
+        print(
+            "\"railtypes.pnml\" not found.  It should be in \"src\" and contain the railtypetable block"
+        )
         return -1
     if not src_directory.joinpath("templates.pnml").exists():
-        print("\"templates.pnml\" not found.  Assuming no templates are required")
+        print(
+            "\"templates.pnml\" not found.  Assuming no templates are required"
+        )
 
     print("Project structure is correct")
     return 0
 
 
-def copy_file(filepath : Path, nml_file : str):
+def copy_file(filepath: Path, nml_file: str):
     if not filepath.exists():
         print("The file <%s> does not exist" % str(filepath))
         return -1
@@ -42,22 +51,22 @@ def copy_file(filepath : Path, nml_file : str):
     return nml_file
 
 
-def find_pnml_files(src_directory : Path):
+def find_pnml_files(src_directory: Path):
     file_list = []
     for path in src_directory.rglob("*.pnml"):
         if path.stem in ["railtypes", "grf", "templates"]:
             continue
         file_list.append(path)
-    
-    print("Found the following files: %s" % [str(file.stem + file.suffix) for file in file_list])
+
+    print("Found the following files: %s" %
+          [str(file.stem + file.suffix) for file in file_list])
     return file_list
 
 
-def write_file(filename : str, nml_file : str):
-    filepath = Path(filename)
+def write_file(filename: str, nml_file: str):
+    filepath = Path(filename + ".nml")
     if filepath.exists():
         print("%s already exists.  Overwriting" % filename)
-
 
     with open(filepath, "w") as file_writer:
         for line in nml_file:
@@ -66,14 +75,15 @@ def write_file(filename : str, nml_file : str):
     return 0
 
 
-def main():
-    src_directory = Path("./src")
-    lang_directory = Path("./lang")
-    gfx_directory = Path("./gfx")
+def main(grf_name, src_dir, lang_dir, gfx_dir, compile_grf):
+    src_directory = Path("src")
+    lang_directory = Path("lang")
+    gfx_directory = Path("gfx")
 
     nml_file = ""
 
-    if check_project_structure(src_directory, gfx_directory, lang_directory) != 0:
+    if check_project_structure(src_directory, gfx_directory,
+                               lang_directory) != 0:
         return -1
 
     nml_file = copy_file(src_directory.joinpath("grf.pnml"), nml_file)
@@ -85,19 +95,39 @@ def main():
         print("Reading '%s'" % (file.stem + file.suffix))
         nml_file = copy_file(file, nml_file)
 
-    if write_file(argv[1], nml_file) != 0:
+    if write_file(argv[-1], nml_file) != 0:
         print("The nml file failed to compile")
         return -1
 
-    # Evil but it works
-    compile_command = "nmlc -l lang " + argv[1]
-    system(compile_command)
+    if compile_grf:
+        found_nml = util.find_spec("nml")
+        if found_nml is not None:
+            from PIL import Image
+            import nml.main
+            nml.main.main(["--lang", str(lang_dir), grf_name + ".nml"])
+            return 1
+        else:
+            print("NML is not installed")
+            return -2
     return 0
 
 
 if __name__ == "__main__":
-    error_code = main()
-    if (error_code != 0):
-        print("The program failed to execute properly.  Please consult the log")
+    parser = ArgumentParser(description="Compile pnml files into one nml file")
+    parser.add_argument("grf_name")
+    parser.add_argument("--src", default="src", help="Source files directory")
+    parser.add_argument("--lang", default="lang", help="Language files directory")
+    parser.add_argument("--gfx", default="gfx", help="Graphics files directory")
+    parser.add_argument("--compile", action="store_true", help="Compile the nml file with nmlc")
+    args = parser.parse_args()
+
+    error_code = main(args.grf_name, args.src, args.lang, args.gfx, args.compile)
+    if error_code == -1:
+        print(
+            "The nml file failed to compile properly.  Please consult the log")
+    elif error_code == -2:
+        print("The nml file comipled correctly, but nml failed to compile it")
+    elif error_code == 1:
+        print("The grf file was compiled successfully")
     else:
-        print("grf file compiled successfully")
+        print("The nml file was compiled successfully")
