2654db1ee3440896bf858705f646a412d9a2be44
Alex Garrity <garrity2799@gmail.com>
2020-05-26 13:12:04 +0100

    Build script can now run the game

--- build.py
+++ build.py
@@ -99,6 +99,52 @@ def compile_grf(has_lang_dir, grf_name, lang_dir):
         return -2
 
 
+def run_game(grf_name):
+    from subprocess import run
+    from sys import platform
+
+    if platform.startswith("linux"):
+        newgrf_dir = Path.home().joinpath(".openttd", "newgrf")
+        executable_path = "/usr/bin/openttd"
+    elif platform.startswith("win32"):
+        newgrf_dir = Path.home().joinpath("Documents", "OpenTTD", "newgrf")
+        executable_path = "C:/Program Files/OpenTTD/openttd.exe"
+
+    json_read_ok = True
+    if Path("build.json").exists():
+        from json import load
+        with open("build.json") as json_data:
+            data = load(json_data)
+            try:
+                newgrf_dir = data["newgrf_dir"]
+                executable_path = data["executable"]
+            except KeyError:
+                print("The config json file is invalid")
+                json_read_ok = False
+
+    if not json_read_ok:
+        from json import dump
+        from os import access, X_OK
+        while not Path(newgrf_dir).exists():
+            newgrf_dir = input("Enter the newgrf directory: ")
+            if newgrf_dir[-6:] != "newgrf":
+                newgrf_dir = "~/.openttd/newgrf"
+
+        while not Path(executable_path).exists():
+            executable_path = input("Enter the OpenTTD executable path: ")
+            if not (access(executable_path[-6], X_OK)):
+                executable_path = "/usr/bin/openttd"
+
+        with open("build.json", "w") as json_data:
+            data = {"newgrf_dir": newgrf_dir, "executable": executable_path}
+            dump(data, json_data)
+
+    from shutil import copy
+    copy(grf_name + ".grf", Path(newgrf_dir))
+    run(executable=executable_path, args="")
+    return 2
+
+
 def main(grf_name, src_dir, lang_dir, gfx_dir, b_compile_grf, b_run_game):
     src_directory = Path("src")
     lang_directory = Path("lang")
@@ -126,8 +172,15 @@ def main(grf_name, src_dir, lang_dir, gfx_dir, b_compile_grf, b_run_game):
         print("The nml file failed to compile")
         return -1
 
-    if b_compile_grf:
-        return compile_grf(has_lang_dir, grf_name, lang_dir)
+    if b_compile_grf or b_run_game:
+        error = compile_grf(has_lang_dir, grf_name, lang_dir)
+        if error == -2:
+            return -2
+        elif b_run_game == False:
+            return 1
+
+    if b_run_game:
+        return run_game(grf_name)
 
     return 0
 
@@ -145,6 +198,10 @@ if __name__ == "__main__":
     parser.add_argument("--compile",
                         action="store_true",
                         help="Compile the nml file with nmlc")
+    parser.add_argument(
+        "--run",
+        action="store_true",
+        help="Run the game after compilation (will also compile the file)")
     args = parser.parse_args()
 
     error_code = main(args.grf_name, args.src, args.lang, args.gfx,
@@ -154,7 +211,13 @@ if __name__ == "__main__":
             "The nml file failed to compile properly.  Please consult the log")
     elif error_code == -2:
         print("The nml file compiled correctly, but nml failed to compile it")
+    elif error_code == -3:
+        print(
+            "The grf file compiled successfully but the game failed to start")
     elif error_code == 1:
         print("The grf file was compiled successfully")
+    elif error_code == 2:
+        print(
+            "The grf file was compiled successfully and the game was started")
     else:
         print("The nml file was compiled successfully")
